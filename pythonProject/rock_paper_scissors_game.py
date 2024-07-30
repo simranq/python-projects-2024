198696bdcd03e901e9be319c864e710afd4a7139
simranq <151132129+simranq@users.noreply.github.com>
2024-07-30 15:10:51 +0530

    Update rock_paper_scissors_game.py

--- pythonProject/rock_paper_scissors_game.py
+++ pythonProject/rock_paper_scissors_game.py
@@ -23,18 +23,22 @@ scissors ="""
 game_images = [rock, paper, scissors]
 print("What do you choose? Type 0 for ROCK, 1 for PAPER or 2 for SCISSORS")
 choice = int(input("You chose: "))
-print(game_images[choice])
-computer_choice = random.randint(0,2)
-print("Computer chose: ")
-print(game_images[computer_choice])
 
 if choice >2 or choice <0:
     print("INVALID NUMBER ENTERED\nTry Again")
-elif choice == 0 and computer_choice == 2:
-    print("You Win!!")
-elif choice == 2 and computer_choice == 0:
-    print("You Lose!!")
-elif choice > computer_choice :
-    print ("You Win!!")
+    
 else :
-    print("You Lose!!")
+    print(game_images[choice])
+    
+    computer_choice = random.randint(0,2)
+    print("Computer chose: ")
+    print(game_images[computer_choice])
+
+    if choice == 0 and computer_choice == 2:
+        print("You Win!!")
+    elif choice == 2 and computer_choice == 0:
+        print("You Lose!!")
+    elif choice > computer_choice :
+        print ("You Win!!")
+    else :
+        print("You Lose!!")
