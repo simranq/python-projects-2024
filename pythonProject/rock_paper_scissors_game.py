8bccd4bf87ac7c76ec06963454a6cca6bd924ffe
simranq <151132129+simranq@users.noreply.github.com>
2024-07-30 15:05:06 +0530

    Update rock_paper_scissors_game.py

--- pythonProject/rock_paper_scissors_game.py
+++ pythonProject/rock_paper_scissors_game.py
@@ -1,28 +1,40 @@
 import random
-
+rock = """
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)"""
+paper ="""
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)"""
+scissors ="""
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)"""
+game_images = [rock, paper, scissors]
 print("What do you choose? Type 0 for ROCK, 1 for PAPER or 2 for SCISSORS")
 choice = int(input("You chose: "))
+print(game_images[choice])
 computer_choice = random.randint(0,2)
+print("Computer chose: ")
+print(game_images[computer_choice])
 
-print(f"Computer chose: {computer_choice}")
-
-if choice == 0 and computer_choice == 0:
-    print("It's a TIE!!")
-elif choice == 0 and computer_choice == 1:
-    print("You lose")
+if choice >2 or choice <0:
+    print("INVALID NUMBER ENTERED\nTry Again")
 elif choice == 0 and computer_choice == 2:
-    print("You win!")
-elif choice == 1 and computer_choice ==0:
-    print("You win!")
-elif choice == 1 and computer_choice == 1:
-    print("It's a TIE!!")
-elif choice == 1 and computer_choice == 2:
-    print("You lose")
-elif choice == 2 and computer_choice == 2:
-    print("It's a TIE!!")
+    print("You Win!!")
 elif choice == 2 and computer_choice == 0:
-    print("You lose")
-elif choice == 2 and computer_choice == 1:
-    print("You win!")
+    print("You Lose!!")
+elif choice > computer_choice :
+    print ("You Win!!")
 else :
-    print("INVALID NO. ENTERED\n RETRY")
+    print("You Lose!!")
