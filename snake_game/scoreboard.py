b7575a35e1f736a2027786d09184ab84b5de6ff0
simranq <151132129+simranq@users.noreply.github.com>
2024-08-22 13:06:51 +0530

    Updated scoreboard.py by saving previous highscores

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -8,6 +8,11 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+       #file read,write  operations are used over here so that the last highscore entry is maintained
+        # well and saved for further gameplay even after reinitiating the whole game again
+        with open("data") as data:
+            self.highscore = int(data.read())
+        #self.highscore = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -15,13 +20,23 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score}    High Score: {self.highscore}", align=ALIGNMENT, font=FONT)
+        with open("data",'w') as data:
+            data.write(f"{self.highscore}")
 
+
+    def reset(self):
+        if self.score > self.highscore :
+            self.highscore = self.score
+        self.score=0
+        self.update_scoreboard()
+    '''
     def game_over(self):
         self.goto(0, 0)
         self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
+   '''
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
