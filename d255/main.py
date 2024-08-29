658d1e7b27f7d2f47e7ab2e80c0e5b7add10d1d7
simranq <151132129+simranq@users.noreply.github.com>
2024-08-29 12:18:08 +0530

    Updated  main.py
    simplified code using list comprehension

--- d255/main.py
+++ d255/main.py
@@ -14,11 +14,15 @@ guessed_states = []
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state's name?").title()
-    if answer_state == "Exit":
-        missing_states = []
+        if answer_state == "Exit":
+        missing_states = [state for state in all_states if state not in guessed_states]
+        """
+        applied list comprehension(refer d26)
         for state in all_states:
             if state not in guessed_states:
                 missing_states.append(state)
+                
+        """
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
